a8100e1c86f58ca4d6ce281a101fef3901b77de3
MateenShaikh1320 <47075820+MateenShaikh1320@users.noreply.github.com>
2020-09-27 14:10:38 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def sayhello(request):
-    return HttpResponse("Hello World")
+    return HttpResponse("<h2> Welcome! This is a Django demo project deployed on a DigitalOcean droplet running Ubuntu 20.04 64 bit LTS server using nginx and gunicorn </h2>")
